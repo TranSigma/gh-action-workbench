fe49b00f6895c72fccac154b017f73a15dccb006
PKilcommons <patrick.kilcommons@transigma.com>
2023-06-30 05:12:35 +0000

    Add dependency and `dep-pkg` CI

--- dep-pkg/dep_pkg/__init__.py
+++ dep-pkg/dep_pkg/__init__.py
@@ -1,8 +1,9 @@
-'''Some local dependency.'''
+"""Some local dependency."""
 import structlog
 
 logger = structlog.stdlib.get_logger()
 
+
 def some_func() -> bool:
-    logger.info("I'm a function in dep-pkg.")
+    logger.info("I'm a function in dep-pkg.", file=__name__)
     return True
--- pyrs/python/pyrs/__main__.py
+++ pyrs/python/pyrs/__main__.py
@@ -1,11 +1,13 @@
-from pyrs import sys_tz, rs_sys_tz
+from pyrs import rs_sys_tz, sys_tz
 
 
 def main():
     py_tz = sys_tz()
     rs_tz = rs_sys_tz()
-    print(f'Local timezone according to:\nPython: {py_tz} of type {type(py_tz)}\n'
-          f'Rust: {rs_tz} of type {type(rs_tz)}')
+    print(  # noqa: T201
+        f"Local timezone according to:\nPython: {py_tz} of type {type(py_tz)}\nRust: {rs_tz} of type {type(rs_tz)}"
+    )
+
 
 if __name__ == "__main__":
     main()
